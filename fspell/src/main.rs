6a9dd73e5d446bc0f4bf9f718752de536b17bfca
Andy <andy@andy.codes>
2022-06-08 14:49:35 +0000

    added a new structure

--- fspell/src/main.rs
+++ fspell/src/main.rs
@@ -2,16 +2,28 @@ use std::fs::{self, File};
 use std::io::{self, BufRead};
 use std::path::Path;
 
+struct Spell {
+    title: String,
+    code: String,
+    category: String,
+}
+
 fn main() {
     let spell_files =
         fs::read_dir("../spells/").expect("Something went wrong while reading directory.");
 
+    let spell_list: Vec<Spell> = Vec::new();
+
     for file in spell_files {
-        println!("{:?}", file);
         if let Ok(lines) = read_lines(file.unwrap().path()) {
+            let new_spell: bool = false;
+            let current_category: String = String::from("");
+
             for line in lines {
                 if let Ok(line) = line {
-                    println!("{}", line);
+                    if line.starts_with("# ") {
+                        // split and assign the value
+                    }
                 }
             }
         }
