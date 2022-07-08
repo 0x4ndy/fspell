5cb869af834e369da02a03cdef7ba3735b610ea2
Andy <andy@andy.codes>
2022-07-08 19:51:04 +0000

    added generation of structs

--- fspell/src/main.rs
+++ fspell/src/main.rs
@@ -6,26 +6,59 @@ struct Spell {
     title: String,
     code: String,
     category: String,
+    sub_category: String,
 }
 
 fn main() {
     let spell_files =
         fs::read_dir("../spells/").expect("Something went wrong while reading directory.");
 
-    let spell_list: Vec<Spell> = Vec::new();
+    let mut spell_list: Vec<Spell> = Vec::new();
 
     for file in spell_files {
-        if let Ok(lines) = read_lines(file.unwrap().path()) {
-            let new_spell: bool = false;
-            let current_category: String = String::from("");
+        if let Ok(lines) = read_lines(file.as_ref().unwrap().path()) {
+            let current_category = String::from(file.unwrap().path().file_stem().unwrap().to_str().unwrap());
+            let mut in_spell = false;
+            let mut current_sub_category = String::from("");
+            let mut current_title = String::from("");
+            let mut current_code = String::from("");
 
-            for line in lines {
+            for (i, line) in lines.enumerate() {
                 if let Ok(line) = line {
                     if line.starts_with("# ") {
-                        // split and assign the value
+                        match line.as_str().split_once(" ") {
+                            None => panic!("Line {} is marked as category name, but it doesn't contain any value.", i),
+                            Some((mark, sub_cat)) => current_sub_category = String::from(sub_cat),
+                        }
+                    } else if line.starts_with("### ") {
+                        match line.as_str().split_once(" ") {
+                            None => panic!("Line {} is marked as spell title, but it doesn't contain any value.", i),
+                            Some((mark, title)) => {
+                                current_title = String::from(title);
+                                in_spell = true;
+                            },
+                        }
+                    } else if line == "---" {
+                        let spell = Spell {
+                           title: String::from(&current_title),
+                           code: String::from(&current_code),
+                           category: String::from(&current_category),
+                           sub_category: String::from(&current_sub_category),
+                        };
+
+                        spell_list.push(spell); 
+
+                        in_spell = false;
+                        current_code = String::from("");
+                    } else if !line.starts_with("```") && in_spell {
+                        current_code.push_str(format!("{}\n", line).as_str());
                     }
                 }
             }
+        }
+
+        for spell in &spell_list {
+            println!("{}:{} - {}\n{}", spell.category, spell.sub_category, spell.title, spell.code);
         }
     }
 }
