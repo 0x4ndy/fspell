0b829300c05f68dc1b0acd8a84283461d484433c
= <=>
2022-07-17 21:56:52 +0200

    split functionalities into seperate functions

--- fspell/src/main.rs
+++ fspell/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::{self, File, ReadDir};
+use std::fs::{self, File, ReadDir, DirEntry};
 use std::io::{self, BufRead};
 use std::path::Path;
 
@@ -7,6 +7,7 @@ struct Spell {
     code: String,
     category: String,
     sub_category: String,
+    file_name: String,
 }
 
 fn main() {
@@ -16,62 +17,82 @@ fn main() {
     let spell_list: Vec<Spell> = files_to_spells(spell_files);
 
     for spell in &spell_list {
-        println!("{}:{} - {}\n{}", spell.category, spell.sub_category, spell.title, spell.code);
+        println!("{}: {} - {}\nFile: {}\n{}", spell.category, spell.sub_category, spell.title, spell.file_name, spell.code);
     }
+}
+
+fn lines_to_spells(file: DirEntry, lines: io::Lines<io::BufReader<File>>) -> Vec<Spell> {
+    let mut spell_list: Vec<Spell> = Vec::new();
+    let current_category = String::from(
+        file
+        .path()
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap());
+
+    let mut in_spell = false;
+    let mut current_sub_category = String::from("");
+    let mut current_title = String::from("");
+    let mut current_code = String::from("");
+
+    for (i, line) in lines.enumerate() {
+        if let Ok(line) = line {
+            if line.starts_with("# ") {
+                match line.as_str().split_once(" ") {
+                    None => panic!("Line {} is marked as category name, but it doesn't contain any value.", i),
+                    Some((_, sub_cat)) => current_sub_category = String::from(sub_cat),
+                }
+            } else if line.starts_with("### ") {
+                match line.as_str().split_once(" ") {
+                    None => panic!("Line {} is marked as spell title, but it doesn't contain any value.", i),
+                    Some((_, title)) => {
+                        current_title = String::from(title);
+                        in_spell = true;
+                    },
+                }
+            } else if line == "---" {
+                let spell = Spell {
+                   title: String::from(&current_title),
+                   code: String::from(&current_code),
+                   category: String::from(&current_category),
+                   sub_category: String::from(&current_sub_category),
+                   file_name: String::from("file"),
+                };
+
+                spell_list.push(spell); 
+
+                in_spell = false;
+                current_code = String::from("");
+            } else if !line.starts_with("```") && in_spell {
+                current_code.push_str(format!("{}\n", line).as_str());
+            }
+        }
+    }
+
+    spell_list
 }
 
 fn files_to_spells(dir_content: ReadDir) -> Vec<Spell> {
     let mut spell_list: Vec<Spell> = Vec::new();
      
-    for file in dir_content {
-        if let Ok(lines) = read_lines(file.as_ref().unwrap().path()) {
-            let current_category = String::from(
-                file.unwrap()
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap());
+    for dir_entry in dir_content {
 
-            let mut in_spell = false;
-            let mut current_sub_category = String::from("");
-            let mut current_title = String::from("");
-            let mut current_code = String::from("");
+        let file = match dir_entry {
+            Err(e) => panic!("Can't process the file: {e}"),
+            Ok(file) => file,
+        };
 
-            for (i, line) in lines.enumerate() {
-                if let Ok(line) = line {
-                    if line.starts_with("# ") {
-                        match line.as_str().split_once(" ") {
-                            None => panic!("Line {} is marked as category name, but it doesn't contain any value.", i),
-                            Some((mark, sub_cat)) => current_sub_category = String::from(sub_cat),
-                        }
-                    } else if line.starts_with("### ") {
-                        match line.as_str().split_once(" ") {
-                            None => panic!("Line {} is marked as spell title, but it doesn't contain any value.", i),
-                            Some((mark, title)) => {
-                                current_title = String::from(title);
-                                in_spell = true;
-                            },
-                        }
-                    } else if line == "---" {
-                        let spell = Spell {
-                           title: String::from(&current_title),
-                           code: String::from(&current_code),
-                           category: String::from(&current_category),
-                           sub_category: String::from(&current_sub_category),
-                        };
+        let file_path = file.path();
 
-                        spell_list.push(spell); 
+        let lines = match read_lines(file_path) {
+            Err(e) => panic!("{e}"),
+            Ok(lines) => lines,
+        };
 
-                        in_spell = false;
-                        current_code = String::from("");
-                    } else if !line.starts_with("```") && in_spell {
-                        current_code.push_str(format!("{}\n", line).as_str());
-                    }
-                }
-            }
-        }
+        let mut spells = lines_to_spells(file, lines);
 
+        spell_list.append(&mut spells);
     }
 
     spell_list
